fe12848993f02f96a60ddfba52bc7423e14b0f4c
Jake Hesford <roguedispatch@users.noreply.github.com>
2023-08-05 08:07:49 +0000

    fixed leap yeap file

--- test_leap.c
+++ test_leap.c
@@ -7,14 +7,14 @@
  * `year` is a leap year or not.
  */
 int is_leap(long year) {
-
-  if (year % 4 != 0) {
-    return 0;
-  }
-
-  if (year % 100 == 0) {
-    return 1;
-  }
+  if (year % 4 == 0) {
+    if (year % 100 == 0) {
+      if (year % 400 != 0){
+        return 0;
+      }
+    }
+      return 1;
+    }
 
   return 0;
 }
